0cd82f02cb50d651b60c8252bddcfd6d742f20ef
YamWillie <33632555+YamWillie@users.noreply.github.com>
2018-11-07 10:27:31 -0500

    Revert "section 2 done"
    This reverts commit a3eb09bcc5bf99980edb8f86aa926dd59b46802d.

--- src/index.js
+++ src/index.js
@@ -1,60 +1,34 @@
-import "phaser";
+import 'phaser';
 
-//Configuration
 var config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0 }
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
     }
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
 };
 
 var game = new Phaser.Game(config);
 
-function preload() {
-  //Loading Assets
-  this.load.image("tiles", "../assets/assets.png");
-  this.load.tilemapTiledJSON("map", "../assets/level1.json");
-  this.load.image("background", "../assets/water.png");
-}
-function create() {
-  //Map
-  const map = this.make.tilemap({ key: "map" });
-  const tileset = map.addTilesetImage("assets", "tiles");
-
-  //Add
-  this.add.image(0, 0, "background");
-
-  //Layers
-  const lowerLayer = map.createStaticLayer("Below Player -1", tileset, 0, 0);
-  const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
-  const grassLayer = map.createStaticLayer("Mushroom-flower", tileset, 0, 0);
-  const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-  const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
-
-  //Layer Properties
-  lowerLayer.setCollisionByProperty({ collides: true });
-  belowLayer.setCollisionByProperty({ collides: true });
-  worldLayer.setCollisionByProperty({ collides: true });
-  //   aboveLayer.setDepth(10);
-
-  //Debug
-  //   const debugGraphics = this.add.graphics().setAlpha(0.75);
-  //   worldLayer.renderDebug(debugGraphics, {
-  //     tileColor: null, // Color of non-colliding tiles
-  //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-  //     faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-  //   });
+function preload ()
+{
+    this.load.image('logo', 'assets/logo.png');
 }
 
-function update() {}
+function create ()
+{
+    var logo = this.add.image(400, 150, 'logo');
+
+    this.tweens.add({
+        targets: logo,
+        y: 450,
+        duration: 2000,
+        ease: 'Power2',
+        yoyo: true,
+        loop: -1
+    });
+
+}
